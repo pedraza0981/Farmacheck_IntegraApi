3c4162d63941d31a7408660cc4baf913fe968013
pedraza0981 <pedraza0981@hotmail.com>
2025-07-04 23:04:07 -0600

    Integrate Subbrand controller with API client

--- Farmacheck.Application/DTOs/SubmarcaDto.cs
+++ Farmacheck.Application/DTOs/SubmarcaDto.cs
@@ -1,16 +1,13 @@
-namespace Farmacheck.Models
+using System;
+
+namespace Farmacheck.Application.DTOs
 {
-    public class SubMarca
+    public class SubmarcaDto
     {
         public int Id { get; set; }
         public int MarcaId { get; set; }
-        public string Nombre { get; set; }
-
-        public string? MarcaNombre { get; set; }
-
+        public string Nombre { get; set; } = null!;
         public bool? Estatus { get; set; }
-
         public DateTime? ModificadaEl { get; set; }
-
     }
 }
