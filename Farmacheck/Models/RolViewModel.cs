c3c1fcc570b4b7dee9b42bd44852d520b2fcd33e
pedraza0981 <pedraza0981@hotmail.com>
2025-07-23 23:52:56 -0600

    upload roles
    upload roles

---
+++ Farmacheck/Models/RolViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Farmacheck.Models
+{
+    public class RolViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public bool Estatus { get; set; }
+
+        public int UnidadDeNegocioId { get; set; }
+
+        public int UnidadDeNegocioNombre { get; set; } 
+
+        public List<int> Permisos { get; set; } = new List<int>();
+    }
+}
