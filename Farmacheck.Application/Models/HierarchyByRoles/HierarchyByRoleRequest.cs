f711d9db76b50cf826e919cb51285d53c4429878
pedraza0981 <pedraza0981@hotmail.com>
2025-07-27 22:33:51 -0600

    se agrega estatus
    se agrega estatus

--- Farmacheck.Application/Models/HierarchyByRoles/HierarchyByRoleRequest.cs
+++ Farmacheck.Application/Models/HierarchyByRoles/HierarchyByRoleRequest.cs
@@ -5,5 +5,7 @@ namespace Farmacheck.Application.Models.HierarchyByRoles
         public string Nombre { get; set; } = null!;
         public int RolSuperiorId { get; set; }
         public int RolSubordinadoId { get; set; }
+
+        public bool Estatus { get; set; }
     }
 }
