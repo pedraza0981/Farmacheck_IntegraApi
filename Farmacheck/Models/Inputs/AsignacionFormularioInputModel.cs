9e4b1253b0da87e673165c2b5907bbb207e06003
pedraza0981 <pedraza0981@hotmail.com>
2025-08-28 12:17:25 -0600

    Revert "Merge pull request #139 from pedraza0981/codex/cambiar-select-a-multiselect-en-formularios"
    This reverts commit bce61e9b755faba59fb584a921807077c44c165b, reversing
    changes made to 344f5440cc3c1f4fad9a5071e0ff96ad8d96f538.

--- Farmacheck/Models/Inputs/AsignacionFormularioInputModel.cs
+++ Farmacheck/Models/Inputs/AsignacionFormularioInputModel.cs
@@ -1,12 +1,10 @@
-using System.Collections.Generic;
-
 namespace Farmacheck.Models.Inputs
 {
     public class AsignacionFormularioInputModel
     {
         public int FormularioId { get; set; }
-        public List<int> Rol1 { get; set; } = new();
+        public int? Rol1 { get; set; }
         public int? Rol2 { get; set; }
-        public List<int> Rol3 { get; set; } = new();
+        public int? Rol3 { get; set; }
     }
 }
