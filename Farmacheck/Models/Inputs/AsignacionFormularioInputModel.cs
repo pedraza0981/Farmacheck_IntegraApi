110e7de6d30ecdbf44629680452668cc8a53cd07
pedraza0981 <pedraza0981@hotmail.com>
2025-08-28 00:37:50 -0600

    Allow multiple role assignment selections

--- Farmacheck/Models/Inputs/AsignacionFormularioInputModel.cs
+++ Farmacheck/Models/Inputs/AsignacionFormularioInputModel.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
+
 namespace Farmacheck.Models.Inputs
 {
     public class AsignacionFormularioInputModel
     {
         public int FormularioId { get; set; }
-        public int? Rol1 { get; set; }
+        public List<int> Rol1 { get; set; } = new();
         public int? Rol2 { get; set; }
-        public int? Rol3 { get; set; }
+        public List<int> Rol3 { get; set; } = new();
     }
 }
