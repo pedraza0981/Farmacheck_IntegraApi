d969f0654f0143d41af016f6b4a23f30ae526304
pedraza0981 <pedraza0981@hotmail.com>
2025-10-01 01:02:48 -0600

    Add login password update modal flow

--- Farmacheck/Controllers/SecurityController.cs
+++ Farmacheck/Controllers/SecurityController.cs
@@ -1,16 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Farmacheck.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Farmacheck.Controllers
 {
     public class SecurityController : Controller
     {
+        private readonly IUserApiClient _userApiClient;
+
+        public SecurityController(IUserApiClient userApiClient)
+        {
+            _userApiClient = userApiClient;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ChangePasswordModal()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> CheckPasswordUpdate(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return Json(new { success = false, error = "Correo electrónico no proporcionado." });
+            }
+
+            try
+            {
+                var user = await _userApiClient.GetUserByEmailAsync(email);
+                if (user is null)
+                {
+                    return Json(new { success = false, error = "Usuario no encontrado." });
+                }
+
+                return Json(new { success = true, data = new { user.ActualizaPass } });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, error = ex.Message });
+            }
         }
     }
 }
