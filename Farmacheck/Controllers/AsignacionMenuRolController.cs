0885a816e6c92fadd5282268607b38a3de88f278
pedraza0981 <pedraza0981@hotmail.com>
2025-10-08 21:32:16 -0600

    update

---
+++ Farmacheck/Controllers/AsignacionMenuRolController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Farmacheck.Controllers
+{
+    public class AsignacionMenuRolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
